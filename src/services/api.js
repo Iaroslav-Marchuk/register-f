460c856e4a10ac915e10a4780e38024c906b324c
iaroslav_marchuk <jarik.13a@gmail.com>
2026-03-01 20:08:20 +0000

    add work

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = 'https://vidreira-backend.onrender.com';
+// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://vidreira-register.onrender.com/';
 
 const axiosAPI = axios.create({
   baseURL: BASE_URL,
